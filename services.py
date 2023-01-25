135073a72c772697b4f12323de692d22b3953e46
omid.pak <omid.pakshekar.est@gmail.com>
2023-01-26 00:36:59 +0330

    add session

--- services.py
+++ services.py
@@ -4,6 +4,11 @@ import database
 def create_db():
     return database.Base.metadata.create_all(bind=database.engine)
 
-
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db 
+    finally:
+        db.close()
 
 create_db()
